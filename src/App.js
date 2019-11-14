8bfc3917c8f35c98dca7d1dc7bc250774eef2b88
kl <kl@debian>
2019-11-14 22:33:23 +0800

    Implement code splitting

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
 import Page1 from "./components/Page1";
-import Page2 from "./components/Page2";
-import Page3 from "./components/Page3";
-// import { tsConstructorType } from "@babel/types";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      route: "page1"
+      route: "page1",
+      component: ""
     };
   }
 
   onRouteChange = route => {
-    this.setState({ route: route });
+    if (route === "page1") {
+      this.setState({ route: route });
+    } else if (route === "page2") {
+      import("./components/Page2").then(Page2 => {
+        console.log(Page2);
+        this.setState({ route: route, component: Page2.default });
+      });
+    } else if (route === "page3") {
+      import("./components/Page3").then(Page3 => {
+        this.setState({ route: route, component: Page3.default });
+      });
+    }
   };
 
   render() {
-    switch (this.state.route) {
-      case "page1":
-        return <Page1 onRouteChange={this.onRouteChange} />;
-      case "page2":
-        return <Page2 onRouteChange={this.onRouteChange} />;
-      case "page3":
-        return <Page3 onRouteChange={this.onRouteChange} />;
-      default:
-        return <Page1 onRouteChange={this.onRouteChange} />;
+    if (this.state.route === "page1") {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange} />;
     }
   }
 }
